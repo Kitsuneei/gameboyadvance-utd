6c0ac3cbb784b0b05be869be8b084c9d784c1179
Mhaj23 <54248377+Mhaj23@users.noreply.github.com>
2023-02-16 09:09:31 -0600

    Added Bare Bones Pseudocode File

---
+++ GBAPsuedocode.c
@@ -0,0 +1,24 @@
+﻿#ifndef GBAEmulator
+#define GBAEmulator
+
+/* Data Structures */
+
+typedef structure {
+
+	//register
+	
+	//memory values for: 
+	iWRAM (internal)
+	vRAM (video)
+	eWRAM (external)
+	gROM (Game PAK ROM)
+	gRAM (Game PAK RAM)
+	
+} 
+
+//function prototypes
+void GBAInitilize
+void GBALoadROM
+	
+
+#endif
